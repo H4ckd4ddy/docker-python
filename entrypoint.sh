c84f3fe8349cb26809a9ef8ce9091ae05e4a32a3
Etienne Sellan <git@sellan.fr>
2019-10-27 09:36:19 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p /app
+
+if [ -z "$GIT_REPO" ]
+then
+	git pull "$GIT_REPO" /app
+fi
+
+if [ -f "/app/requirements.txt" ]
+	pip3 install -r /app/requirements.txt
+fi
+
+if [ -z "$APP_ENTRYPOINT" ]
+then
+	cd /app
+	python3 -u "$APP_ENTRYPOINT"
+else
+	echo "Please specify APP_ENTRYPOINT"
+fi
