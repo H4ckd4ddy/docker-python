efc256781591358f3d0e3999cf027f3f61ecd714
Etienne Sellan <git@sellan.fr>
2020-01-24 15:17:04 +0100

    Add apt requirements install option

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 mkdir -p /app
 cd /app
 
+if [ ! -z "$APT_REQUIREMENTS" ]
+then
+	apt update
+	apt install -y "$APT_REQUIREMENTS"
+fi
+
 if [ -d "/app/.git" ]
 then
 	git pull
@@ -13,7 +19,8 @@ fi
 
 if [ -f "/app/requirements.txt" ]
 then
-	pip3 install -r /app/requirements.txt
+	pip install --upgrade pip
+	pip install --upgrade -r /app/requirements.txt
 fi
 
 if [ ! -z "$APP_ENTRYPOINT" ]
